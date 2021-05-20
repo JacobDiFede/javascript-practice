eedd03d0bdc1016fc95aa4e47c682ff2d3d00b6c
Jacob DiFede <novix1234@gmail.com>
2021-05-19 21:10:45 -0700

    Day 2

--- README.md
+++
@@ -1,3 +0,0 @@
-# javascript-practice
-BitWise Javascript Practice
-Learning to code JavaScript with BitWise
---
+++ day-2/js/data.js
@@ -0,0 +1,16 @@
+// Primitive Data Types 
+const appName = '0';
+const score = 0;
+const isVIP = true;
+
+
+// Compound Data Types 
+
+const array = [1, '1', true]
+const object = {
+    appName: '1',
+    score: 1,
+    isVIP: false,
+};
+
+console.log(array[0], object.score);
