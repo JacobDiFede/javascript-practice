3febd33bc26e647ab68c2a0e9f3714ae179871db
Jacob DiFede <novix1234@gmail.com>
2021-06-01 01:48:23 -0700

    Small changes

--- day-5/js/tower.js
+++ day-5/js/tower.js
@@ -1,5 +1,5 @@
 function createTower() {
-    
+
     const tower = {
 
     };
@@ -7,7 +7,7 @@ function createTower() {
 }
 
 function createEnemy() {
-    
+
     const enemy = {
         health: 3,
         element: document.createElement('div'),
@@ -17,18 +17,18 @@ function createEnemy() {
             return this.health;
         },
         createElement() {
-            this.element.innerHTML = 'enemy'
+            this.element.innerHTML = '.';
             document.body.appendChild(this.element);
             this.element.style.position = 'absolute';
             this.element.style.left = 0;
-            this.myInterval = setInterval( () => {
+            this.myInterval = setInterval(  () => {
                 console.log(this);
                 this.move()
             }, 2000 );
         },
         move() {
             console.log(this.getLeft())
-            this.element.style.left = parseInt(this.getLeft()) + 20 +'px';
+            this.element.style.left = parseInt(this.getLeft()) + 20 + 'px';
             if (parseInt(this.getLeft()) > 200) {
                 alert('you lost');
                 clearInterval(this.myInterval);
@@ -39,25 +39,33 @@ function createEnemy() {
         },
         takeDamage() {
             this.health -= 1;
-            if (this.health <1) {
+            if (this.health < 1) {
+            
                 clearInterval(this.myInterval);
             }
         }
+
+
     };
     return enemy;
 }
+
 
 const enemies = [];
 
 setInterval(function () {
     const enemy1 = createEnemy();
-    enemy1. readHealth();
+    enemy1.readHealth();
     enemy1.createElement();
     enemies.push(enemy1);
 }, 2000);
 
+
+
 document.addEventListener('click', () => {
-    enemies.forEach ( enemy => {
+
+    enemies.forEach( enemy => {
         enemy.takeDamage();
     })
-})
+    
+})  
