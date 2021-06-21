bdb072e150eb3e8eae011398832b50fa1f412263
Jacob DiFede <novix1234@gmail.com>
2021-06-21 10:34:13 -0700

    Progress

--- 8Ball-Final-Project/js/main.js
+++ 8Ball-Final-Project/js/main.js
@@ -1,1 +1,8 @@
 alert('Work in Progress!')
+
+const questionButton = document.querySelector('#sendQuestion');
+
+questionButton.addEventListener( 'click', () => {
+    const questionLog = question.value;
+    console.log(questionLog);
+});
